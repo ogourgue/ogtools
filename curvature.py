f7f030c2cd4fdf9f5235e7f3377f2c98e2e3872e
Olivier Gourgue <ogourgue@gmail.com>
2018-08-30 15:49:51 +0200

    curvature: new module (empty so far) to compute gaussian curvature of a P1 field
    to be used to compute curvature of telemac bathymetry

---
+++ curvature.py
@@ -0,0 +1,15 @@
+# author: O. Gourgue (University of Antwerp, Belgium)
+
+import numpy as np
+
+
+
+def neighborhood():
+
+  print 'to be done'
+
+
+
+def gaussian_curvature_p1(x, y, z, ngb):
+
+  print 'to be done'
